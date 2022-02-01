90a79f6b5ba6f8ee1fb7693e54b95c5d391510f0
Kundan Kumar <kr070999@gmail.com>
2022-02-01 09:48:31 +0530

    added function and their headers

--- math.py
+++ math.py
@@ -1,1 +1,15 @@
-print("mathematics")
+# Addition
+def sum(x,y):
+    pass
+
+# Subtraction  
+def sub(x,y):
+    pass
+
+# Multiplication  
+def mul(x,y):
+    pass
+
+# Division   
+def div(x,y):
+    pass
